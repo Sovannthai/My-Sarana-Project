5e012aa03d0713eac13ccb3612a1815892ed7d8a
PheakdeySmith <pheakdeysmith@gmail.com>
2024-08-10 23:41:03 +0700

    RouteHelper and Amenity API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Helpers\Routes\RouteHelper;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')
+    ->group(function () {
+        RouteHelper::includeRouteFiles(__DIR__ . '/api/v1');
+});
+
---
+++ routes/api/v1/amenities.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AmenityController;
+
+Route::middleware('auth')
+    ->name('amenity.')
+    ->group(function () {
+
+        Route::get('amenities', [AmenityController::class, 'index'])->name('index')->withoutMiddleware('auth');
+        Route::get('amenities/{amenity}', [AmenityController::class, 'show'])->name('show')->withoutMiddleware('auth');
+        Route::post('amenities', [AmenityController::class, 'store'])->name('store')->withoutMiddleware('auth');
+        Route::patch('amenities/{amenity}', [AmenityController::class, 'update'])->name('update')->withoutMiddleware('auth');
+        Route::delete('amenities/{amenity}', [AmenityController::class, 'delete'])->name('delete')->withoutMiddleware('auth');
+
+});
